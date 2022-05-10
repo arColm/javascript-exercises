371c046808e25da3aa8f6422be11e58375704382
pokerdf <jacob.leung@mail.mcgill.ca>
2022-05-10 16:28:56 -0400

    ex 6,7 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(typeof year!=="number") {
+        return "ERROR";
+    }
 
+
+    if(year % 4 ===0) {
+        if(year % 100 === 0) {
+            if(year % 400 ===0) {
+                return true;
+            } else return false;
+        } else return true;
+    } else return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result = (temp -32) * 5 / 9;
+  result *=10;
+  return (Math.round(result)/10);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result = (temp * 9 / 5) +32;
+  result *=10;
+  return (Math.round(result)/10);
 };
 
 // Do not edit below this line
