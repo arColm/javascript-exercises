5171f87b71a8154c7f6740f730e4c06d144068f3
pokerdf <jacob.leung@mail.mcgill.ca>
2022-05-11 19:32:04 -0400

    ex8 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let result = 0;
+  while(numArray.length!==0) {
+    result+=parseInt(numArray.pop());
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+	let result = 1;
+  while(numArray.length!==0) {
+    result*=parseInt(numArray.pop());
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	let result=num;
+  for(let i=1;i<power;i++){
+    result*=parseInt(num);
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result=1;
+	for(let i=2;i<=num;i++) {
+    result*=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
