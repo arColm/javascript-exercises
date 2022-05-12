0b7027b816b1fb1f33dfbd99bdc35f4ebe74c774
pokerdf <jacob.leung@mail.mcgill.ca>
2022-05-11 21:22:19 -0400

    ex 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibonacciArray = [1,1];
+    if(num<0) return "OOPS";
 
+    for(let i=2;i<num;i++){
+        fibonacciArray.push(fibonacciArray[i-2]+fibonacciArray[i-1]);
+    }
+    return fibonacciArray[num-1];
 };
 
 // Do not edit below this line
