78b414696150c801e011a8b7d8ba6961c595a5a6
pokerdf <jacob.leung@mail.mcgill.ca>
2022-05-11 22:13:11 -0400

    ex 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    console.log(new Date().getFullYear());
+    return people.reduce((oldestPerson,currentPerson) => {
+        let currentPersonAge = 0;
+        let oldestPersonAge = 0;
+        if(currentPerson.yearOfDeath!==undefined) {
+            currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        } else {
+            currentPersonAge = new Date().getFullYear() - currentPerson.yearOfBirth;
+        }
+        
+        if(oldestPerson.yearOfDeath!==undefined) {
+            oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        } else {
+            oldestPersonAge = new Date().getFullYear() - oldestPerson.yearOfBirth;
+        }
+        
+        if(currentPersonAge > oldestPersonAge) {
+            return currentPerson;
+        } else return oldestPerson;
 
+        
+    })
 };
 
 // Do not edit below this line
