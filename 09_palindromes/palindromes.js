8afc7304b3be2075bd53af7d5d1fe65034016279
pokerdf <jacob.leung@mail.mcgill.ca>
2022-05-11 21:15:47 -0400

    ex 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,52 @@
-const palindromes = function () {
 
+
+const palindromes = function (string) {
+    const illegalCharString = ",./?! ";
+    const illegalCharArray = Array.from(illegalCharString);
+    let charArray = Array.from(string);
+    while(charArray.length!==0){
+        let validChar=false;
+        let char = "a";
+        let firstChar="a";
+        //Find last char that is legal
+        while(!validChar){
+            char=charArray.pop();
+            validChar=true;
+            //Check if char is illegal.
+            for(let i=0;i<illegalCharArray.length;i++){
+                if(illegalCharArray[i]===char){
+                    validChar=false;
+                    break;
+                }
+            }
+        }
+        validChar = false;
+        //Find first char that is legal
+        while(!validChar) {
+            firstChar=charArray.shift();
+            validChar=true;
+            //Check if char is illegal.
+            for(let i=0;i<illegalCharArray.length;i++){
+                if(illegalCharArray[i]===firstChar){
+                    validChar=false;
+                    break;
+                }
+            }
+        }
+        if(firstChar !==undefined && char.toLowerCase()!==firstChar.toLowerCase()){
+            console.log(charArray);
+            return false;
+        }
+
+
+    }
+
+
+
+    return true;
 };
 
+
+palindromes('racecar')
 // Do not edit below this line
 module.exports = palindromes;
